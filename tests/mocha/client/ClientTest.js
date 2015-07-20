222a12231d64ea12487f67ccaa59d7b9e7b166b0
Jack Singleton <git@jacksingleton.com>
2015-07-19 17:26:11 -0700

    doesnt look like checks for MochaWeb being present are needed

--- tests/mocha/client/ClientTest.js
+++ tests/mocha/client/ClientTest.js
@@ -1,9 +1,7 @@
-if (!(typeof MochaWeb === 'undefined')){
-  MochaWeb.testOnly(function(){
-    describe("Client Test", function(){
-      it("should work", function(){
-        chai.expect(true).to.equal(true);
-      });
+MochaWeb.testOnly(function(){
+  describe("Client Test", function(){
+    it("should work", function(){
+      chai.expect(true).to.equal(true);
     });
   });
-}
+});
