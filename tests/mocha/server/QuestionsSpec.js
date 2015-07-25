d56c61796353c8bef16b1d4f23e855d628700e63
Jack Singleton <git@jacksingleton.com>
2015-07-25 16:44:22 -0700

    move submissions test to new file and delete SubmitAnswersSpec because it was just testing the Meteor api

--- tests/mocha/server/QuestionsSpec.js
+++ tests/mocha/server/QuestionsSpec.js
@@ -15,29 +15,5 @@ MochaWeb.testOnly(function() {
       // Then
       chai.expect(questionIds).to.have.members([id]);
     });
-
-    it("should give us answers back with order matching the given questions", function() {
-      // Given
-      var questions = [
-        { _id: 'id-1', text: 'aaa' },
-        { _id: 'id-2', text: 'bbb' },
-        { _id: 'id-3', text: 'ccc' }
-      ];
-
-      Submissions.remove({});
-      Submissions.insert({ answers: [
-        { questionId: 'id-2', answer: 'udfn answer 2'},
-        { questionId: 'id-1', answer: 'zl3k answer 1'},
-        { questionId: 'id-3', answer: 'akdf answer 3'}
-      ]});
-
-      // When
-      var answersArray = Submissions.inTableFormat(questions);
-
-      // Then
-      chai.expect(answersArray).to.deep.have.members([
-        ['zl3k answer 1', 'udfn answer 2', 'akdf answer 3']
-      ]);
-    });
   });
 });
