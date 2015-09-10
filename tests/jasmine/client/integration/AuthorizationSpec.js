b83935048d2bee7a6020d91163c7096b217c0917
Jack Singleton <git@jacksingleton.com>
2015-09-09 23:15:19 -0700

    allow anyone to insert Prompts and test that owners can add a prompt

--- tests/jasmine/client/integration/AuthorizationSpec.js
+++ tests/jasmine/client/integration/AuthorizationSpec.js
@@ -1,55 +1,48 @@
 describe("authorization", function() {
 
-  function withUser(permissions, f) {
-    var ownerId = Accounts.createUser({
-      email: 'test@test.com',
-      password: 'testing',
-      services: {
-        sandstorm: {
-          permissions: permissions
-        }
+  function withSandstormUser(f) {
+    HTTP.get("/.sandstorm-credentials", function (error, result) {
+      if (error) {
+        console.error(error.stack);
+      } else if (!result.data) {
+        console.error("/.sandstorm-credentials is not JSON?");
+      } else if (result.data.token) {
+        Meteor.loginWithToken(result.data.token, f);
       }
     });
-
-    Meteor.loginWithPassword("test@test.com", "testing", function(err) {
-      expect(err).toBeUndefined();
-      f();
-    });
-  }
-
-  function withNonOwner(f) {
-    withUser([], f);
-  }
-
-  function withOwner(f) {
-    withUser(['owner'], f);
   }
 
   beforeEach(function() {
-    Prompts.find().forEach( function (p) {
+    Prompts.find().forEach(function (p) {
       Prompts.remove({_id: p._id});
     });
   });
 
-  it("should not let a non-owner add Prompts", function(done) {
-    withNonOwner(function() {
-      Prompts.insert("Should anyone be able to add prompts?", function(error, result) {
+  //it("should not let a non-owner add Prompts", function(done) {
+  //  withSandstormUser(function() {
+  //    Prompts.insert("Should anyone be able to add prompts?", function(error, result) {
 
-        expect(error.error).toBe(403);
+  //      // TODO: not checking for undefined error first passes? what?
+  //      expect(error).not.toBeUndefined();
+  //      expect(error.error).toBe(403);
+
+  //      done();
+  //    });
+  //  });
+  //});
+
+  it("should let an owner add Prompts", function (done) {
+    // Given
+    withSandstormUser(function() {
+      // When
+      Prompts.insert("Should owners be able to add prompts?", function(error, result) {
+
+        // Then
+        expect(error).toBeUndefined();
 
         done();
       });
     });
   });
 
-  //it("should let an owner add Prompts", function (done) {
-  //  withOwner(function() {
-  //    Prompts.insert("Should anyone be able to add prompts?", function(error, result) {
-
-  //      expect(error).toBeUndefined();
-
-  //      done();
-  //    });
-  //  });
-  //});
 });
