0d7c99ab7251e859d22d5b36759b883d362f37a5
Jack Singleton <git@jacksingleton.com>
2015-09-09 22:43:42 -0700

    add option to test.sh to run only client unit, client integration, or server integration tests

--- test.sh
+++ test.sh
@@ -15,4 +15,30 @@ if [ ${DEBUG-0} -ne "0" ]; then
   export VELOCITY_DEBUG_MIRROR=1
 fi
 
+export JASMINE_SERVER_UNIT=0
+export JASMINE_SERVER_INTEGRATION=0
+export JASMINE_CLIENT_UNIT=0
+export JASMINE_CLIENT_INTEGRATION=0
+
+case "$1" in
+  cu)
+    export JASMINE_CLIENT_UNIT=1
+    ;;
+  ci)
+    export JASMINE_CLIENT_INTEGRATION=1
+    ;;
+  si)
+    export JASMINE_SERVER_INTEGRATION=1
+    ;;
+  su)
+    echo "As of Sep 9, server unit tests not supported according to:" 1>&2
+    echo "https://velocity.readme.io/docs/jasmine-testing-modes#section-server-unit-test-mode" 1>&2
+    exit 1
+    ;;
+  *)
+    export JASMINE_SERVER_INTEGRATION=1
+    export JASMINE_CLIENT_UNIT=1
+    export JASMINE_CLIENT_INTEGRATION=1
+esac
+
 meteor --test --release velocity:METEOR@1.1.0.2_3
