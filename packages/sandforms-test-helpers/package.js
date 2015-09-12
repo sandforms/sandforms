8c9dc222fa349957e9feba34c3a25977cd689f02
Jack Singleton <git@jacksingleton.com>
2015-09-12 16:28:15 -0700

    allow everyone to see all prompts

--- packages/sandforms-test-helpers/package.js
+++ packages/sandforms-test-helpers/package.js
@@ -1,13 +1,20 @@
 Package.describe({
+<<<<<<< HEAD
   version: '1.0.0',
+=======
+>>>>>>> allow everyone to see all prompts
   debugOnly: true
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
+<<<<<<< HEAD
   api.addFiles('server/test-helpers.js', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('test-helpers');
+=======
+  api.addFiles('test-helpers.js');
+>>>>>>> allow everyone to see all prompts
 });
