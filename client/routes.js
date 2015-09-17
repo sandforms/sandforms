258ef3999bd45f3961daf2212dab52e756de2a42
Jack Singleton <git@jacksingleton.com>
2015-09-16 20:31:36 -0700

    Jack </3: Move isOwner to new User object and add lib folder

--- client/routes.js
+++ client/routes.js
@@ -1,18 +1,9 @@
-Router.isOwner = function(user) {
-  if (user === null) {
-    return false;
-  } else {
-    var permissions = user.services.sandstorm.permissions;
-    return _(permissions).contains("owner");
-  }
-}
-
 Router.configure({
   layoutTemplate: 'index'
 });
 
 Router.route('/', function() {
-  if (Router.isOwner(Meteor.user())) {
+  if (User.isOwner(Meteor.user())) {
     this.render('create');
   } else {
     this.render('submit');
