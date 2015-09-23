f596a6fc71e077f5c6b37b5a3823f22bfe44b0f8
Scatchell <Scatchell@gmail.com>
2015-09-23 12:27:38 -0700

    Anthony <3 Rosalie: forcing meteor to precompile sass assets for CI

--- test.sh
+++ test.sh
@@ -49,6 +49,9 @@ esac
 
 true || pkill -f meteor
 
-mv ./stylesheets/{,.}index.scss
+# So we need meteor to run twice because our scss assets need to
+# be precompiled before they can be used by meteor. This is gross
+# and we are sorry. There are no meteor precompile run options.
+meteor --test 2>/dev/null || true
+
 meteor --test --release velocity:METEOR@1.1.0.2_3
-mv ./stylesheets/{.,}index.scss
