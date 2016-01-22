50af89b763d3efc4100720c39d02686465e1d42c
Michael Chaffee <mchaffee@thoughtworks.com>
2016-01-22 14:47:30 -0800

    Berke & Michael: Refine keypress event; add focus/blur color change to Submit button

--- submit/client/submit.js
+++ submit/client/submit.js
@@ -25,7 +25,7 @@ Template.submit.events({
     Router.go('/thanks')
   },
 
-  'keypress .input-field': function(e) {
+  'keypress .response-input': function(e) {
     var activeInput = e.target;
     var isEnterKey = e.keyCode == 13;
     var inputs = $(activeInput).closest('form').find(':input');
@@ -35,9 +35,21 @@ Template.submit.events({
       e.preventDefault();
       inputs.eq( inputs.index(activeInput)+ 1 ).focus();
     }
+  },
+
+  'focus .btn': function(e) {
+    var button = e.target;
+    if( button.id == 'submit-responses') {
+      button.style.backgroundColor = 'black';   
+    }
+  },
+
+  'blur .btn': function(e) {
+    var button = e.target;
+    if (button.id == 'submit-responses') {
+      button.style.backgroundColor = '';
+    }
   }
-  // TODO: Maybe form should post when user hits Enter on final input if all inputs are filled?
-  // TODO: Submit button should have same focus/blur behavior as current hover/no-hover activity
 });
 
 Template.submit.onRendered(function() {
