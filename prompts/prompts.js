02034dd1dd64fcb06657659c6db30214a4669f5b
Chandni Patel <chandni_patel@gap.com>
2016-04-15 22:04:35 -0700

    Chandni <3 Vlad: Format to please intellij

--- prompts/prompts.js
+++ prompts/prompts.js
@@ -1,70 +1,71 @@
 Prompts = new Mongo.Collection("prompts");
 
 Prompts.allow({
-  insert: function(userId) {
-    return User.isOwnersUserId(userId);
-  },
-  update: function(userId) {
-    return User.isOwnersUserId(userId);
-  },
-  remove: function(userId) {
-    return User.isOwnersUserId(userId);
-  }
+    insert: function (userId) {
+        return User.isOwnersUserId(userId);
+    },
+    update: function (userId) {
+        return User.isOwnersUserId(userId);
+    },
+    remove: function (userId) {
+        return User.isOwnersUserId(userId);
+    }
 });
 
 if (Meteor.isServer) {
-  Meteor.publish("prompts", function() {
-    return Prompts.find({});
-  });
+    Meteor.publish("prompts", function () {
+        return Prompts.find({});
+    });
 
-  Meteor.methods({
-    'Prompts.create': function(promptText, promptRequired) {
-      var defaultPromptType = "shortAnswer";
+    Meteor.methods({
+        'Prompts.create': function (promptText, promptRequired) {
+            var defaultPromptType = "shortAnswer";
 
-      if (typeof promptRequired === 'undefined') {
-        promptRequired = false;
-      }
-      if (typeof selectedPromptType === 'undefined') {
-        selectedPromptType = defaultPromptType;
-      }
+            if (typeof promptRequired === 'undefined') {
+                promptRequired = false;
+            }
+            if (typeof selectedPromptType === 'undefined') {
+                selectedPromptType = defaultPromptType;
+            }
 
-      check(promptText, String);
-      check(promptRequired, Boolean);
-      var order = incrementCounter(Counters, "promptOrder");
-      if(promptText != "") {
-          return Prompts.insert({text: promptText,
-            required: promptRequired,
-            order: order,
-            selectedPromptType: selectedPromptType,
-            option: ""
-          });
-      }
-    }
-  });
+            check(promptText, String);
+            check(promptRequired, Boolean);
+            var order = incrementCounter(Counters, "promptOrder");
+            if (promptText != "") {
+                return Prompts.insert({
+                    text: promptText,
+                    required: promptRequired,
+                    order: order,
+                    selectedPromptType: selectedPromptType,
+                    option: ""
+                });
+            }
+        }
+    });
 }
 
-Prompts.create = function(promptText, promptRequired) {
-  promptRequired = (typeof promptRequired === 'undefined') ? false: promptRequired;
-  return Meteor.call('Prompts.create', promptText, promptRequired);
+Prompts.create = function (promptText, promptRequired) {
+    promptRequired = (typeof promptRequired === 'undefined') ? false : promptRequired;
+    return Meteor.call('Prompts.create', promptText, promptRequired);
 };
 
-Prompts.allPromptIds = function(maybeOptions) {
-  var queryObject = _buildQueryFromOptions(maybeOptions);
-  return Prompts.find(queryObject).map(function(prompt) {
-    return prompt._id;
-  });
+Prompts.allPromptIds = function (maybeOptions) {
+    var queryObject = _buildQueryFromOptions(maybeOptions);
+    return Prompts.find(queryObject).map(function (prompt) {
+        return prompt._id;
+    });
 };
 
-Prompts.inOrder = function(maybeOptions) {
-  var queryObject = _buildQueryFromOptions(maybeOptions);
-  return Prompts.find(queryObject, {sort: ['order']}).fetch();
+Prompts.inOrder = function (maybeOptions) {
+    var queryObject = _buildQueryFromOptions(maybeOptions);
+    return Prompts.find(queryObject, {sort: ['order']}).fetch();
 };
 
-Prompts.markAsDeleted = function(promptId) {
-  Prompts.update(
-    { _id: promptId },
-    { $set: { "deleted": true } }
-  );
+Prompts.markAsDeleted = function (promptId) {
+    Prompts.update(
+        {_id: promptId},
+        {$set: {"deleted": true}}
+    );
 };
 
 Prompts.updateOption = function (promptId, text) {
@@ -78,20 +79,20 @@ Prompts.updateOption = function (promptId, text) {
 
 
 function _buildQueryFromOptions(maybeOptions) {
-  var options = _(
-    nullOrUndefined(maybeOptions) ? {} : maybeOptions
-  ).defaults({
-    deleted : false
-  });
+    var options = _(
+        nullOrUndefined(maybeOptions) ? {} : maybeOptions
+    ).defaults({
+        deleted: false
+    });
 
-  var queryObject = {};
+    var queryObject = {};
 
-  if (!options.deleted) {
-    queryObject['$or'] = [
-      { "deleted": { $exists: false }},
-      { "deleted": false }
-    ];
-  }
+    if (!options.deleted) {
+        queryObject['$or'] = [
+            {"deleted": {$exists: false}},
+            {"deleted": false}
+        ];
+    }
 
-  return queryObject;
+    return queryObject;
 }
--- tests/jasmine/server/integration/PromptsSpec.js
+++ tests/jasmine/server/integration/PromptsSpec.js
@@ -1,133 +1,133 @@
-describe("prompts", function() {
+describe("prompts", function () {
 
-  it("should let us get all prompt ids", function() {
-    // Given
-    Prompts.remove({});
-    var id = Prompts.create('What is your favorite color?');
+    it("should let us get all prompt ids", function () {
+        // Given
+        Prompts.remove({});
+        var id = Prompts.create('What is your favorite color?');
 
-    // When
-    var promptIds = Prompts.allPromptIds();
+        // When
+        var promptIds = Prompts.allPromptIds();
 
-    // Then
-    expect(promptIds.length).toBe(1);
-    expect(promptIds).toContain(id);
-  });
+        // Then
+        expect(promptIds.length).toBe(1);
+        expect(promptIds).toContain(id);
+    });
 
-  it("should not store empty prompts", function() {
-      // Given
-      Prompts.remove({});
-      var id = Prompts.create('');
+    it("should not store empty prompts", function () {
+        // Given
+        Prompts.remove({});
+        var id = Prompts.create('');
 
-      // When
-      var promptIds = Prompts.allPromptIds();
+        // When
+        var promptIds = Prompts.allPromptIds();
 
-      // Then
-      expect(promptIds).not.toContain(id);
-  });
+        // Then
+        expect(promptIds).not.toContain(id);
+    });
 
-  it("create should increment the order each time we call it", function() {
-    // Given
-    Prompts.remove({});
-    Counters.remove({});
+    it("create should increment the order each time we call it", function () {
+        // Given
+        Prompts.remove({});
+        Counters.remove({});
 
-    Prompts.create('hello');
-    Prompts.create('world');
+        Prompts.create('hello');
+        Prompts.create('world');
 
-    // When
-    var promptId = Prompts.create('the third prompt');
+        // When
+        var promptId = Prompts.create('the third prompt');
 
-    // Then
-    var order = Prompts.findOne(promptId).order
-    expect(order).toBe(3);
-  });
+        // Then
+        var order = Prompts.findOne(promptId).order
+        expect(order).toBe(3);
+    });
 
-  it("should return all prompts in creation order", function() {
-    Counters.remove({})
-    Prompts.remove({});
-    var id1 = Prompts.create('Your favorite book?');
-    var id2 = Prompts.create('What is your favorite color?');
+    it("should return all prompts in creation order", function () {
+        Counters.remove({})
+        Prompts.remove({});
+        var id1 = Prompts.create('Your favorite book?');
+        var id2 = Prompts.create('What is your favorite color?');
 
-    var prompts = Prompts.inOrder();
+        var prompts = Prompts.inOrder();
 
-    expect(prompts[0].text).toEqual('Your favorite book?');
-  });
+        expect(prompts[0].text).toEqual('Your favorite book?');
+    });
 
-  it("should not return deleted prompt ids", function(){
-    // Given
-    Prompts.remove({});
-    var id = Prompts.create('What is your favorite color?');
+    it("should not return deleted prompt ids", function () {
+        // Given
+        Prompts.remove({});
+        var id = Prompts.create('What is your favorite color?');
 
-    // When
-    Prompts.markAsDeleted(id);
+        // When
+        Prompts.markAsDeleted(id);
 
-    // Then
-    var promptIds = Prompts.allPromptIds();
+        // Then
+        var promptIds = Prompts.allPromptIds();
 
-    expect(promptIds.length).toBe(0);
-    expect(promptIds).not.toContain(id);
-  });
+        expect(promptIds.length).toBe(0);
+        expect(promptIds).not.toContain(id);
+    });
 
-  it("should not return deleted prompts when inOrder is called", function() {
-    // Given
-    Prompts.remove({});
-    var id = Prompts.create('What is your favorite color?');
+    it("should not return deleted prompts when inOrder is called", function () {
+        // Given
+        Prompts.remove({});
+        var id = Prompts.create('What is your favorite color?');
 
-    // When
-    Prompts.markAsDeleted(id);
+        // When
+        Prompts.markAsDeleted(id);
 
-    // Then
-    var prompts = Prompts.inOrder();
+        // Then
+        var prompts = Prompts.inOrder();
 
-    expect(prompts.length).toBe(0);
-    expect(prompts).not.toContain(id);
-  });
+        expect(prompts.length).toBe(0);
+        expect(prompts).not.toContain(id);
+    });
 
-  it("should return deleted and undeleted prompts when the deleted option is passed to inOrder", function() {
-    // Given
-    Prompts.remove({});
-    var id = Prompts.create('What is your favorite color?');
-    var id2 = Prompts.create('This is the second question');
+    it("should return deleted and undeleted prompts when the deleted option is passed to inOrder", function () {
+        // Given
+        Prompts.remove({});
+        var id = Prompts.create('What is your favorite color?');
+        var id2 = Prompts.create('This is the second question');
 
-    // When
-    Prompts.markAsDeleted(id);
+        // When
+        Prompts.markAsDeleted(id);
 
-    // Then
-    var prompts = Prompts.inOrder({ deleted: true });
+        // Then
+        var prompts = Prompts.inOrder({deleted: true});
 
-    expect(prompts.length).toBe(2);
-  });
+        expect(prompts.length).toBe(2);
+    });
 
-  it("should default Required to false", function() {
-    Prompts.remove({});
-    var promptText = "Why is the sky blue?";
-    var id = Prompts.create(promptText);
-    var prompt = Prompts.inOrder()[0];
-    expect(prompt.text).toEqual(promptText);
-    expect(prompt.required).toEqual(false);
-  });
+    it("should default Required to false", function () {
+        Prompts.remove({});
+        var promptText = "Why is the sky blue?";
+        var id = Prompts.create(promptText);
+        var prompt = Prompts.inOrder()[0];
+        expect(prompt.text).toEqual(promptText);
+        expect(prompt.required).toEqual(false);
+    });
 
-  it("should set the Required property of a prompt", function() {
-    Prompts.remove({});
-    var promptText = "Why is the sky blue?";
-    var promptText2 = "Why do birds sing?";
-    var id = Prompts.create(promptText, true);
-    var id2 = Prompts.create(promptText2, false);
-    var prompts = Prompts.inOrder();
-    var prompt = prompts[0];
-    var prompt2 = prompts[1];
-    expect(prompt.text).toEqual(promptText);
-    expect(prompt2.text).toEqual(promptText2);
-    expect(prompt.required).toEqual(true);
-    expect(prompt2.required).toEqual(false);
-  });
+    it("should set the Required property of a prompt", function () {
+        Prompts.remove({});
+        var promptText = "Why is the sky blue?";
+        var promptText2 = "Why do birds sing?";
+        var id = Prompts.create(promptText, true);
+        var id2 = Prompts.create(promptText2, false);
+        var prompts = Prompts.inOrder();
+        var prompt = prompts[0];
+        var prompt2 = prompts[1];
+        expect(prompt.text).toEqual(promptText);
+        expect(prompt2.text).toEqual(promptText2);
+        expect(prompt.required).toEqual(true);
+        expect(prompt2.required).toEqual(false);
+    });
 
-  it("should default selectedPromptType to shortAnswer", function() {
-    Prompts.remove({});
-    var promptText = "Why is the sky blue?";
-    var id = Prompts.create(promptText);
-    var prompt = Prompts.inOrder()[0];
-    expect(prompt.text).toEqual(promptText);
-    expect(prompt.selectedPromptType).toEqual("shortAnswer");
-  });
+    it("should default selectedPromptType to shortAnswer", function () {
+        Prompts.remove({});
+        var promptText = "Why is the sky blue?";
+        var id = Prompts.create(promptText);
+        var prompt = Prompts.inOrder()[0];
+        expect(prompt.text).toEqual(promptText);
+        expect(prompt.selectedPromptType).toEqual("shortAnswer");
+    });
 
 });
